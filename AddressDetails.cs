41b3f16618e011be890be83714c536aeaa64f6a9
Subhash Barela <barelasubhash16@gmail.com>
2023-03-12 14:30:47 +0530

    Read And Write data from Csv File

---
+++ AddressDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day28ProgramsUsingTPL
+{
+    internal class AddressDetails
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day28ProgramsUsingTPL
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Read Data From Csv File");
+            CsvHandler.CSVDataHndling();
+            Console.ReadLine();
+        }
+    }
+}
